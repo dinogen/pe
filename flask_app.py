68dd0ae1a786c725c59701f03169c2bb31f50cb7
Marcello Semboli <dinogen@gmail.com>
2020-12-18 17:26:33 +0100

    epreprpe

--- flask_app.py
+++ flask_app.py
@@ -2,10 +2,18 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello from Flask!'
+
+@app.route('/user/<int:user_id>')
+def get_user(user_id):
+    user = {"user_id": user_id, "name": "mario"}
+    return json.dumps(user)
+
+
 
